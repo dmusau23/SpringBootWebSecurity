9104b78e899ef4e71be51d550ce6fcfd365ad0f5
dmusau23 <danmusau09@gmail.com>
2025-05-25 21:07:27 -0400

    first commit

---
+++ src/main/java/org/dmusau/springbootdemo/SpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package org.dmusau.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+    public static void main(String[] args) throws Throwable{
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+
+}
